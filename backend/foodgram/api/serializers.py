20c438256a2e6cae15a6dd34ff4313e7e9a6a7c9
Zulfat-Gafurzyanov <gafurzyanov.zulfat@gmail.com>
2025-06-23 12:42:07 +1000

    isort

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -2,7 +2,6 @@ from django.core.exceptions import ObjectDoesNotExist
 from djoser.serializers import UserCreateSerializer
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
-
 from recipes.models import (Favorite, IngredientInRecipe, Ingredients, Recipes,
                             ShoppingCart, Tags, UserRecipe)
 from users.models import MyUser, Subscribes
